af4588ea5047ae0561fa11ea8d0660dd26f025b3
carlodefalco <carlodefalco@users.noreply.github.com>
2020-06-18 11:28:59 +0200

    Add files via upload

---
+++ inst/quadtree.m
@@ -0,0 +1,96 @@
+classdef quadtree < handle
+
+  properties
+    H = 1;
+    W = 1;
+    N = 512;
+    S = sparse (512, 512);
+    dx = 1/512;
+    dy = 1/512;
+    I=[];
+    J=[];
+    V=[];
+    marker = [];
+  endproperties
+
+  methods
+
+    function qt = quadtree (H, W, N)
+      if (nargin > 0)
+        qt.H = H;
+        qt.W = W;
+        qt.N = N;
+        qt.S = sparse (N, N);
+        qt.dx = W/N;
+        qt.dy = H/N;
+      endif
+      qt.S(1,1) = qt.N;
+      qt.refresh ();      
+    endfunction
+
+    function qt = refresh (qt)
+      [qt.I, qt.J, qt.V] = find (qt.S);
+      qt.marker = zeros (size (qt.V));
+    endfunction
+    
+    function n = num_quadrants (qt)
+      n = nnz (qt.S);
+    endfunction
+
+    function [x0, y0, x1, y1] = idx2coord (qt, i, j)
+      x0 = (j-1) * qt.dx;
+      y0 = (i-1) * qt.dy;
+      if (nargout > 2)
+        x1 = x0 + qt.S(i,j) * qt.dx;
+        y1 = y0 + qt.S(i,j) * qt.dy;
+      endif
+    endfunction
+
+    function [i, j] = num2idx (qt, k)
+      [i, j] = deal (qt.I(k), qt.J(k));
+    endfunction
+
+    function refine (qt, k)
+      [i, j] = num2idx (qt, k);
+      s = qt.S(i,j);
+      if s > 1
+        s /= 2;
+        qt.S(i, j) = s;
+        qt.S(i+s, j) = s;
+        qt.S(i, j+s) = s;
+        qt.S(i+s, j+s) = s;
+      endif
+      
+    endfunction
+
+    function h = draw_quadrant (qt, k)
+      [i, j, v] = deal (qt.I(k), qt.J(k), qt. V(k));
+      [x0, y0, x1, y1] = idx2coord (qt, i, j)
+      h = plot ([x0, x1, x1, x0, x0], [y0, y0, y1, y1, y0], 'k');
+    endfunction    
+
+    function draw (qt)
+      for k = 1 : qt.num_quadrants ()
+        draw_quadrant (qt, k);
+        hold on
+      endfor 
+    endfunction
+
+    function paint (qt)
+      I = full (qt.S);
+      I(qt.N+1,qt.N+1)=0;
+      [x0, y0] = meshgrid (linspace (0, qt.W, qt.N+1), linspace (0, qt.H, qt.N+1));
+      for k = 1 : qt.num_quadrants ()
+        [i, j, v] = deal (qt.I(k), qt.J(k), qt. V(k));        
+        I(i-1+(1:v), j-1+(1:v)) = v;  
+      endfor
+      hold on
+      h = pcolor (x0, y0, I);
+      set (h, 'edgecolor', 'none');
+      qt.draw ();
+      I
+    endfunction
+    
+  endmethods
+  
+endclassdef
